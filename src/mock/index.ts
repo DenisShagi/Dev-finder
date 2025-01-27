f6de713fb8dc757455721352569117da4dcfbf09
Denis <denis.shagim@icloud.com>
2025-01-27 11:18:32 +0300

    feat: create info component

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "../types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "DenisShagi",
+  id: 149366145,
+  avatar: "https://avatars.githubusercontent.com/u/149366145?v=4",
+  name: "Denis",
+  company: "Epam",
+  blog: "",
+  location: "Russia, Moscow",
+  bio: "",
+  twitter: "",
+  repos: 21,
+  followers: 1,
+  following: 0,
+  created: "2023-10-30T08:06:31Z",
+};
